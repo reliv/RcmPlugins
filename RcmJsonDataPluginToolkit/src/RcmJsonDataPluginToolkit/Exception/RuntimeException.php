cb40d25c72cec667212a266a6ee5604fc12167b0
Rod Mcnew <rmcnew@relivinc.com>
2012-11-05 18:05:17 -0600

    continue building customer application

---
+++ RcmJsonDataPluginToolkit/src/RcmJsonDataPluginToolkit/Exception/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rmcnew
+ * Date: 11/5/12
+ * Time: 4:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace RcmJsonDataPluginToolkit\Exception;
+class RuntimeException extends \RuntimeException
+{
+
+}
