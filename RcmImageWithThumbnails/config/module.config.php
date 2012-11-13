c59d0cdd6606d211bad8d8c5f104866edb97d2e2
Brian Janish <bjanish@relivinc.com>
2012-11-13 15:11:43 -0600

    Made final changes to script

---
+++ RcmImageWithThumbnails/config/module.config.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * ZF2 Plugin Config file
+ *
+ * This file contains all the configuration for the Module as defined by ZF2.
+ * See the docs for ZF2 for more information.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: No License yet
+ *
+ * @category  Reliv
+ * @package   RcmPlugins\RcmImageWithThumbnails
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   http://www.nolicense.com None
+ * @version   GIT: <git_id>
+ * @link      http://ci.reliv.com/confluence
+ */
+
+return array(
+
+    'rcmPlugin' => array(
+        'RcmImageWithThumbnails'=>array(
+            'type' => 'Basic',
+            'display' => 'Image With Thumbnails',
+            'tooltip' => 'Editable box with three text areas and a background image',
+            'icon' => '',
+            'editJs'=>'/modules/rcm-image-with-thumbnails/edit.js',
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+);
