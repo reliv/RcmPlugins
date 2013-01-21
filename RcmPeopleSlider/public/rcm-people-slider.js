d90d7df9e4f82785459d936a77faa8ef969457b4
Rod Mcnew <rmcnew@relivinc.com>
2013-01-21 11:31:05 -0600

    complete view portion of people slider

--- RcmPeopleSlider/public/rcm-people-slider.js
+++ RcmPeopleSlider/public/rcm-people-slider.js
@@ -28,46 +28,66 @@ var RcmPeopleSlider = function (instanceId, instanceConfig) {
     var apertureSlider = new ApertureSlider(
         container.find('.peopleAperture'),
         {
-            frameWidth : 150,
-            minHeight : 100,
-            frameSeparation : 0,
-            framesPerView : framesPerView,
-            hideOffScreenFrames : false
+            frameWidth:160,
+            minHeight:140,
+            frameSeparation:0,
+            framesPerView:framesPerView,
+            hideOffScreenFrames:false
         }
     );
 
     /**
      * Runs immediately after this class is instantiated
      */
-    me.init = function(){
+    me.init = function () {
 
         //Transfer data into DOM for later use and easy saving
         $.each(
             container.find('.person'),
-            function(){
+            function () {
                 var personEle = $(this);
                 var personId = personEle.attr('data-personId');
                 var person = instanceConfig.people[personId];
                 personEle.append(
                     '<table class="details" style="display:none;">' +
                         '<tr>' +
-                            '<td class="longDesc">' +person.longDesc +'</td>' +
-                            '<td>' +
-                                '<img class="largeImage" ' +
-                                'src="' + person.largeImage + '">' +
-                            '</td>' +
+                        '<td class="longDesc">' +
+                        person.longDesc +
+                        '</td>' +
+                        '<td>' +
+                        '<img class="largeImage" ' +
+                        'src="' + person.largeImage + '">' +
+                        '</td>' +
                         '</tr>' +
-                    '</table>'
+                        '</table>'
                 )
             }
         );
 
         container.find('.person').click(me.handlePersonClick);
+        container.find('.left').click(apertureSlider.pageBack);
+        container.find('.right').click(apertureSlider.pageForward);
     };
 
-    me.handlePersonClick = function(){
-        container.find('.mainPerson table').html($(this).find('.details').html());
-        apertureSlider.goForward();
+    me.handlePersonClick = function () {
+        var thisPersonDiv = $(this);
+
+        container.find('.person').removeClass('selected');
+        thisPersonDiv.addClass('selected');
+
+        container.find('.mainPerson table').html(
+            thisPersonDiv.find('.details').html()
+        );
+
+        //PersonId's start with zero but frames start with 1
+        var frame = parseInt(thisPersonDiv.attr('data-personId')) + 1;
+
+        //Put the selected frame more in the center
+        if (frame > 1) {
+            frame = frame - 1;
+        }
+
+        apertureSlider.goToFrame(frame);
     };
 
     me.init();
