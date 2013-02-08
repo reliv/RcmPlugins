589dcbe38bdb9254bc98789480ef88371be190a1
Westin Shafer <wshafer@relivinc.com>
2013-02-08 10:45:26 -0600

    Pusing for Rod to test

---
+++ RcmTabs/config/module.config.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * ZF2 Plugin Config file
+ *
+ * This file contains all the configuration for the Module as defined by ZF2.
+ * See the docs for ZF2 for more information.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: No License yet
+ *
+ * @category  Reliv
+ * @package   RcmPlugins\RcmTabs
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      http://ci.reliv.com/confluence
+ */
+
+return array(
+
+    'rcmPlugin' => array(
+        'RcmTabs'=>array(
+            'type' => 'Containers',
+            'display' => 'Tabbed Container',
+            'tooltip' => 'A tabbed plugin controller.',
+            'icon' => '',
+            'editJs'=>'/modules/rcm-tabs/edit.js',
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+);
---
+++ RcmTabs/public/edit.js
@@ -0,0 +1,125 @@
+/**
+ * RcmPortalAnnouncementBox
+ *
+ * JS for editing RcmPortalAnnouncementBox
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: No License yet
+ *
+ * @category  Reliv
+ * @package   RcmPlugins\RcmPortalAnnouncementBox
+ * @author    Rod McNew <rmcnew@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   http://www.nolicense.com None
+ * @version   GIT: <git_id>
+ * @link      http://ci.reliv.com/confluence
+ */
+var RcmTabsEdit = function (instanceId, container) {
+
+    /**
+     * Always refers to this object unlike the 'this' JS variable;
+     *
+     * @type {RcmPortalAnnouncementBoxEdit}
+     */
+    var me = this;
+
+    /**
+     * jQuery object for the two links
+     *
+     * @type {Object}
+     */
+    var liTags = container.find('a');
+
+    me.tabContainers = [];
+
+    /**
+     * Background image jQuery object
+     *
+     * @type {Object}
+     */
+
+    /**
+     * Called by content management system to make this plugin user-editable
+     */
+    me.initEdit = function(){
+
+       // container.find('.urlContainer').attr('class','dottedeUrlContainer');
+
+        //Double clicking will show properties dialog
+        container.delegate('div', 'dblclick', function(){
+            me.showEditDialog();
+        });
+
+        //Add right click menu
+        rcmEdit.pluginContextMenu({
+            selector:rcm.getPluginContainerSelector(instanceId),
+            //Here are the right click menu options
+            items:{
+                add:{
+                    name:'Add New Tab',
+                    icon:'edit',
+                    callback:function () {
+                        me.showEditDialog();
+                    }
+                }
+
+            }
+        });
+
+    };
+
+    /**
+     * Called by content management system to get this plugins data for saving
+     * on the server
+     *
+     * @return {Object}
+     */
+    me.getSaveData = function () {
+
+        container.find('li').each(function(){
+            me.tabContainers.push({
+                'id' : $(this).attr('data-containerId'),
+                'type' : $(this).attr('data-containerType')
+            })
+        });
+
+        return {
+            'containers': me.tabContainers
+        }
+    };
+
+    me.getAssets = function(){
+        var saveData = me.getSaveData();
+        return [saveData.href];
+    };
+
+    /**
+     * Displays a dialog box to edit href and image src
+     */
+    me.showEditDialog = function () {
+
+        //Create and show our edit dialog
+        var form = $('<form></form>')
+            .addClass('simple')
+            .addInput('href', 'Link Url', aTags.attr('href'))
+            .dialog({
+                title:'Properties',
+                modal:true,
+                width:620,
+                buttons:{
+                    Cancel:function () {
+                        $(this).dialog("close");
+                    },
+                    Ok:function () {
+
+                        //Get user-entered data from form
+                        aTags.attr('href', form.find('[name=href]').val());
+
+                        $(this).dialog("close");
+                    }
+                }
+            });
+
+    };
+};
