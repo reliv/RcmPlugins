4649d2c783aafd10e4c9cd0d9f1caf7f4a3bbc08
Rod Mcnew <rmcnew@relivinc.com>
2013-05-09 16:13:16 -0500

    fix people slider name

--- RcmPeopleSlider/config/module.config.php
+++ RcmPeopleSlider/config/module.config.php
@@ -24,7 +24,7 @@ return array(
     'rcmPlugin' => array(
         'RcmPeopleSlider'=>array(
             'type' => 'Images & Slide Shows',
-            'display' => 'Testimonials',
+            'display' => 'People Slider',
             'tooltip' => 'People Slider',
             'icon' => '',
             'editJs'=>'/modules/rcm-people-slider/rcm-people-slider-edit.js',
