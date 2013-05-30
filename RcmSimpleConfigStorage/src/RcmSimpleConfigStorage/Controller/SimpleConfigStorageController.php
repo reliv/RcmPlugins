0134c8a2a370b160d58b5c4df820206d2240e21d
Rod Mcnew <rmcnew@relivinc.com>
2013-05-30 17:08:04 -0500

    fix comment

--- RcmSimpleConfigStorage/src/RcmSimpleConfigStorage/Controller/SimpleConfigStorageController.php
+++ RcmSimpleConfigStorage/src/RcmSimpleConfigStorage/Controller/SimpleConfigStorageController.php
@@ -217,9 +217,8 @@ class SimpleConfigStorageController
             if(is_array($value)){
                 if(isset($value['0'])){
                     /*
-                     * Numeric arrays ignore default values because o
-                     * of the "more in default
-                     * that on production" issue
+                     * Numeric arrays ignore default values because of the
+                     * "more in default that on production" issue
                      */
                     $default[$key]=$changes[$key];
                 }else{
