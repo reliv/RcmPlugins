2d01c11e2beaec0286a2e4d93b0370f7f51d42ff
brian <brian@janish.org>
2013-11-04 17:10:01 -0600

    global.php - changed cache manager to memcache.
    other files - brightcove integration

---
+++ RcmBrightcovePlayer/autoload_classmap.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'RcmBrightcovePlayer\Controller\PluginController' => __DIR__ . '/src/RcmBrightcovePlayer/Controller/PluginController.php',
+    'RcmBrightcovePlayer\Module' => __DIR__ . '/Module.php',
+);
