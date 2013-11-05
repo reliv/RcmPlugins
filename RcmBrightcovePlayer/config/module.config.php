7c222b073a148d7f45024e813ed973a89ba01361
brian <brian@janish.org>
2013-11-05 09:19:35 -0600

    Added memcache to local.php.staging

---
+++ RcmBrightcovePlayer/config/module.config.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * ZF2 Plugin Config file
+ *
+ * This file contains all the configuration for the Module as defined by ZF2.
+ * See the docs for ZF2 for more information.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: No License yet
+ *
+ * @category  Reliv
+ * @package   RcmPlugins\RcmHtmlArea
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      http://ci.reliv.com/confluence
+ */
+
+return array(
+
+    'rcmPlugin' => array(
+        'RcmBrightcovePlayer' => array(
+            'type' => 'Social Media',
+            'display' => 'Brightcove Video Player',
+            'tooltip' => 'Add a Brightcove Video to the page',
+            'icon' => '',
+            'editJs' => '/modules/rcm-brightcove-player/rcm-brightcove-player-edit.js',
+            'defaultInstanceConfig' => include __DIR__ . '/defaultInstanceConfig.php'
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+);
---
+++ RcmBrightcovePlayer/src/RcmBrightcovePlayer/Controller/PluginController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 10/23/13
+ * Time: 8:54 AM
+ */
+
+namespace RcmBrightcovePlayer\Controller;
+
+
+use RcmSimpleConfigStorage\Controller\SimpleConfigStorageController;
+
+class PluginController extends SimpleConfigStorageController
+{
+
+    /** @var  \Rcm\Entity\User */
+
+    function __construct(
+        \Doctrine\ORM\EntityManager $entityMgr,
+        $config
+    )
+    {
+        parent::__construct($entityMgr, $config);
+    }
+
+    function renderInstance($instanceId)
+    {
+        return parent::renderInstance(
+            $instanceId,
+            array(
+            )
+        );
+    }
+
+    function renderDefaultInstance($instanceId){
+        return parent::renderDefaultInstance(
+            $instanceId,
+            array()
+        );
+    }
+
+}
