48c56069bf15bf4f847f5a163f26f92044eba195
Rod Mcnew <rmcnew@relivinc.com>
2013-12-04 16:35:45 -0600

    breakout json doctrine controller for better testing and extendability

--- RcmDoctrineJsonPluginStorage/test/RcmDoctrineJsonPluginStorageTest/Mock/PluginStorageMgrMock.php
+++ RcmDoctrineJsonPluginStorage/test/RcmDoctrineJsonPluginStorageTest/Mock/PluginStorageMgrMock.php
@@ -1,41 +1,43 @@
+USE GETMOCK INSTEAD SO WE DON'T NEED AN INTERFACE HERE;
+
 <?php
-/**
- * Created by PhpStorm.
- * User: rmcnew
- * Date: 12/4/13
- * Time: 2:14 PM
- */
-
-namespace RcmDoctrineJsonPluginStorageTest\Mock;
-
-class PluginStorageMgrMock
-{
-
-    protected $instanceConfigs = array();
-
-    public function readInstance($instanceId)
-    {
-        return $this->instanceConfigs[$instanceId];
-    }
-
-    public function saveInstance($instanceId, $configData)
-    {
-        $this->instanceConfigs[$instanceId] = $configData;
-    }
-
-    public function deleteInstance($instanceId)
-    {
-        unset($this->instanceConfigs[$instanceId]);
-    }
-
-    public function getDefaultInstanceConfig()
-    {
-        return $this->instanceConfigs[0];
-    }
-
-    public function getInstanceConfig($instanceId, $pluginName)
-    {
-        return $this->instanceConfigs[$instanceId];
-    }
-
-} 
+///**
+// * Created by PhpStorm.
+// * User: rmcnew
+// * Date: 12/4/13
+// * Time: 2:14 PM
+// */
+//
+//namespace RcmDoctrineJsonPluginStorageTest\Mock;
+//
+//class PluginStorageMgrMock
+//{
+//
+//    protected $instanceConfigs = array();
+//
+//    public function readInstance($instanceId)
+//    {
+//        return $this->instanceConfigs[$instanceId];
+//    }
+//
+//    public function saveInstance($instanceId, $configData)
+//    {
+//        $this->instanceConfigs[$instanceId] = $configData;
+//    }
+//
+//    public function deleteInstance($instanceId)
+//    {
+//        unset($this->instanceConfigs[$instanceId]);
+//    }
+//
+//    public function getDefaultInstanceConfig()
+//    {
+//        return $this->instanceConfigs[0];
+//    }
+//
+//    public function getInstanceConfig($instanceId, $pluginName)
+//    {
+//        return $this->instanceConfigs[$instanceId];
+//    }
+//
+//}
