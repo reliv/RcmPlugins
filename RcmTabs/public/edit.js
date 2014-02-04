09596b0af85f5f53fbe86888966ccc531bd4632c
Rod McNew <rmcnew@relivinc.com>
2014-02-04 09:37:24 -0600

    WEB CART
        WEBCART-31
    
    initial changes

--- RcmTabs/public/edit.js
+++ RcmTabs/public/edit.js
@@ -16,11 +16,6 @@
  */
 var RcmTabsEdit = function (instanceId, container) {
 
-    /**
-     * Always refers to this object unlike the 'this' JS variable;
-     *
-     * @type {RcmTabsEdit}
-     */
     var me = this;
 
     me.tabContainers = [];
@@ -29,40 +24,43 @@ var RcmTabsEdit = function (instanceId, container) {
 
     me.instanceId = instanceId;
 
-    me.tabTitleContainer = $(container).find('.rcmTabsUI_'+me.instanceId);
+    me.tabTitleContainer = $(container).find('.rcmTabsUI_' + me.instanceId);
 
     /**
      * Called by content management system to make this plugin user-editable
      */
-    me.initEdit = function(){
+    me.initEdit = function () {
 
         me.refreshTabs();
         me.disablePropigationFromTitles();
-
-        var tabs = container.find(me.tabTitleContainer).find('li');
 
         me.tabCount = container.find(me.tabTitleContainer).find('li').length;
 
         var selector = rcm.getPluginContainerSelector(me.instanceId);
 
-        me.addRightClick(selector+' .rcmTabsUI_'+me.instanceId+' li', true);
+        me.addRightClick(selector + ' .rcmTabsUI_' + me.instanceId + ' li', true);
         me.addRightClick(selector);
 
+        container.find('a').click(me.tabChanged());
     };
-//
-    me.addRightClick = function(selector, addDelete) {
+
+    me.tabChanged = function(){
+        window['rcmEdit'].refreshEditors(container)
+    };
+
+    me.addRightClick = function (selector, addDelete) {
         var items = {
-            add:{
-                name:'Add New Tab',
-                icon:'edit',
-                callback:function () {
+            add: {
+                name: 'Add New Tab',
+                icon: 'edit',
+                callback: function () {
                     me.addTab();
                 }
             },
             sort: {
                 name: 'Sort Tabs',
-                icon:'edit',
-                callback: function() {
+                icon: 'edit',
+                callback: function () {
                     me.refreshTabs(true);
                 }
             }
@@ -72,7 +70,7 @@ var RcmTabsEdit = function (instanceId, container) {
             items.delete = {
                 name: 'Delete Tab',
                 icon: 'delete',
-                callback: function(){
+                callback: function () {
                     me.deleteTab(this)
                 }
             }
@@ -85,12 +83,11 @@ var RcmTabsEdit = function (instanceId, container) {
         });
     };
 
-    me.deleteTab = function(tabToDelete) {
+    me.deleteTab = function (tabToDelete) {
         var tabId = $(tabToDelete).find("a").attr("href");
         $(tabId).remove();
         $(tabToDelete).remove();
         me.refreshTabs();
-        return;
     };
 
     /**
@@ -103,10 +100,10 @@ var RcmTabsEdit = function (instanceId, container) {
 
         me.tabContainers = [];
 
-        $("#RcmRealPage .rcmTabs_"+instanceId).find(me.tabTitleContainer).find('li').each(function(){
+        container.find(me.tabTitleContainer).find('li').each(function () {
             me.tabContainers.push({
-                'id' : $(this).attr('data-containerId'),
-                'type' : $(this).attr('data-containerType')
+                'id': $(this).attr('data-containerId'),
+                'type': $(this).attr('data-containerType')
             })
         });
 
@@ -115,14 +112,10 @@ var RcmTabsEdit = function (instanceId, container) {
         }
     };
 
-    me.getAssets = function(){
-        return null;
-    };
-
     me.showAddDialog = function () {
         var options = {
-            'html' : 'HTML',
-            'video' : 'Bright Cove Video'
+            'html': 'HTML',
+            'video': 'Bright Cove Video'
         };
 
         var tabTypeSelector = $("<div id='tabType'></div>").dialogIn('select', 'Select Type', options, 'html');
@@ -130,14 +123,14 @@ var RcmTabsEdit = function (instanceId, container) {
         var form = $('<form></form>')
             .append(tabTypeSelector)
             .dialog({
-                title:'Add New Tab',
-                modal:true,
-                width:620,
-                buttons:{
-                    Cancel:function () {
+                title: 'Add New Tab',
+                modal: true,
+                width: 620,
+                buttons: {
+                    Cancel: function () {
                         $(this).dialog("close");
                     },
-                    Ok:function () {
+                    Ok: function () {
                         me.addTab();
                         $(this).dialog("close");
                     }
@@ -145,10 +138,8 @@ var RcmTabsEdit = function (instanceId, container) {
             });
     };
 
-    me.addTab = function() {
+    me.addTab = function () {
         var newId = me.tabCount + 2;
-//        var tabType = $("#tabType").val();
-
         var tabType = 'html';
 
         me.addTabTitle(newId, tabType);
@@ -158,7 +149,7 @@ var RcmTabsEdit = function (instanceId, container) {
         }
 
         me.refreshTabs();
-        rcmEdit.refreshEditors(container.parents('[data-rcmplugininstanceid="'+ me.instanceId +'"]'));
+        rcmEdit.refreshEditors(container.parents('[data-rcmplugininstanceid="' + me.instanceId + '"]'));
         me.disablePropigationFromTitles();
 
         me.tabCount = newId;
@@ -171,10 +162,10 @@ var RcmTabsEdit = function (instanceId, container) {
      * @param type
      */
 
-    me.addTabTitle = function(newId, type) {
-        var newLi = $('<li data-containerId="'+newId+'" data-containerType="'+type+'"></li>');
-        var newA = $('<a href="#tab_'+me.instanceId+'_'+newId+'"></a>');
-        var newDiv = $('<div data-richedit="tab_title_'+newId+'">New Tab</div>');
+    me.addTabTitle = function (newId, type) {
+        var newLi = $('<li data-containerId="' + newId + '" data-containerType="' + type + '"></li>');
+        var newA = $('<a href="#tab_' + me.instanceId + '_' + newId + '"></a>');
+        var newDiv = $('<div data-richedit="tab_title_' + newId + '">New Tab</div>');
 
         $(newA).append(newDiv);
         $(newLi).append(newA);
@@ -182,41 +173,43 @@ var RcmTabsEdit = function (instanceId, container) {
         $(container).find(me.tabTitleContainer).append(newLi);
     };
 
-    me.addHtmlTab = function(newId) {
-        var newDiv = $('<div id="tab_'+instanceId+'_'+newId+'"></div>');
-        var newHtmlContainer = $('<div data-richedit="tab_content_'+newId+'"></div>');
+    me.addHtmlTab = function (newId) {
+        var newDiv = $('<div id="tab_' + instanceId + '_' + newId + '"></div>');
+        var newHtmlContainer = $('<div data-richedit="tab_content_' + newId + '"></div>');
         var newDummyData = '<h1>Lorem ipsum</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justosapien, convallis vehicula sollicitudin tristique, rhoncus vel enim.Maecenas mollis dignissim urna, et mollis augue tempus nec. Proin a orcinulla. Pellentesque laoreet orci vitae nisl viverra imperdiet. Nam eueuismod orci. Etiam adipiscing condimentum quam a venenatis. Fusce faucibuslacus non velit varius egestas. Nam molestie mattis sem quis cursus. Duissit amet nunc turpis. Mauris elit urna, dapibus id hendrerit eleifend,placerat at magna. Etiam ornare eleifend elit, vel blandit tellussagittis pretium. Vivamus ut dignissim purus.</p>';
 
         $(newHtmlContainer).html(newDummyData);
         $(newDiv).append(newHtmlContainer);
 
-        $("#RcmRealPage .rcmTabs_"+instanceId).append(newDiv);
+        container.append(newDiv);
     };
 
-    me.disablePropigationFromTitles = function() {
-        $("#RcmRealPage .rcmTabs_"+instanceId).find(me.tabTitleContainer).find("li").find("a").find("div").keydown(function(event){
+    me.disablePropigationFromTitles = function () {
+        container.find(me.tabTitleContainer).find("li").find("a").find("div").keydown(function (event) {
             event.stopPropagation();
         })
-    }
+    };
 
-    me.refreshTabs = function(sortable) {
+    me.refreshTabs = function (sortable) {
 
-        try{
-            $("#RcmRealPage .rcmTabs_"+instanceId).tabs("destroy");
-        } catch (err) {}
+        try {
+            container.tabs("destroy");
+        } catch (err) {
+        }
 
-        var tabs = $("#RcmRealPage .rcmTabs_"+instanceId).tabs();
+        var tabs = container.tabs();
 
-        if (sortable){
-            tabs.find( ".ui-tabs-nav" ).sortable({
+        if (sortable) {
+            tabs.find(".ui-tabs-nav").sortable({
                 axis: "x",
-                stop: function() {
-                    tabs.tabs( "refresh" );
+                stop: function () {
+                    tabs.tabs("refresh");
                 }
             });
         }
 
         me.disablePropigationFromTitles();
+        me.tabChanged();
     };
 
 };
