5c3e7ce9c7a77f94ff38c26413ee4c02f943c4fa
Rod McNew <rmcnew@relivinc.com>
2014-04-24 14:10:38 -0500

    FIX LOGIN    WEBCART-233
        Cart product page plugins - get working with new user and use factory classes

--- RcmLogin/config/defaultInstanceConfig.php
+++ RcmLogin/config/defaultInstanceConfig.php
@@ -1,15 +1,9 @@
 <?php
 return array(
-    'loginHeader' => 'Login',
-    'userNameCopy' => 'Username:',
-    'loginPasswordCopy' => 'Password:',
-    'loginSubmitCopy' => 'login',
-    'bottomLoginText' => '<a href="/reset-password">Forgot your password?</a><br>',
     'translate' => array(
         'invalid' => 'Incorrect Username or Password',
         'missing' => 'Missing Username or Password.',
         'systemFailure' => 'There was an error logging you in. Please try
-        again.',
-        'buttonProcessing' => 'processing...',
+        again.'
     ),
 );
---
+++ RcmPluginHelpers/config/module.config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ZF2 Module Config file for Rcm
+ *
+ * This file contains all the configuration for the Module as defined by ZF2.
+ * See the docs for ZF2 for more information.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: No License yet
+ *
+ * @category  Reliv
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ */
+return array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'aliases' => array(
+                'modules/rcm-plugin-helpers/' => __DIR__ . '/../public/',
+            ),
+        ),
+    ),
+);
