57b7bd62e228616e78a6fcf1799d0112f0780d08
Rod McNew <rmcnew@relivinc.com>
2014-04-24 14:14:50 -0500

    FIX LOGIN    WEBCART-233
        Cart product page plugins - get working with new user and use factory classes

--- RcmPluginHelpers/public/form-double-submit-protect/form-double-submit-protect.js
+++ RcmPluginHelpers/public/form-double-submit-protect/form-double-submit-protect.js
@@ -1,11 +1,11 @@
 /**
- * This finds any form with attribute data-form-processing and ensures that each
+ * This finds any form with attribute data-form-double-submit-protect and ensures that each
  * form can only be submitted once and shows that it is loading via css.
  *
  * setTimeout(func,0) is similar to $.ready()
  */
 setTimeout(function () {
-    var forms = document.querySelectorAll("form[data-form-processing]");
+    var forms = document.querySelectorAll("form[data-form-double-submit-protect]");
     for (var i = 0; i < forms.length; ++i) {
         forms[i].addEventListener('submit', function () {
             if (this.className.indexOf('processing') != -1) {
