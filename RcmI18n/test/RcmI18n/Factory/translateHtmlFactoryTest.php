b2c403a2b7414d7b2cc23d1685d5bfed4be893f0
Rod McNew <rmcnew@relivinc.com>
2014-04-29 16:29:09 -0500

    fix repo

---
+++ RcmI18n/test/RcmI18n/Factory/translateHtmlFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RcmI18nTest\ViewHelper;
+
+use RcmI18n\Factory\TranslateHtmlFactory;
+use Zend\I18n\Translator\LoaderPluginManager;
+use Zend\ServiceManager\ServiceManager;
+
+require __DIR__ . '/../../autoload.php';
+
+class TranslateHtmlFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers RcmI18n\Factory\TranslateHtmlFactory
+     */
+    function testCreateService()
+    {
+        $sm = new ServiceManager();
+        $sm->setService(
+            'RcmHtmlPurifier',
+            $this->getMockBuilder('\HtmlPurifier')
+                ->disableOriginalConstructor()
+                ->setMethods(array('purify'))
+                ->getMock()
+        );
+        $viewSm= new LoaderPluginManager();
+        $viewSm->setServiceLocator($sm);
+        $unit = new TranslateHtmlFactory();
+        $this->assertInstanceOf(
+            'RcmI18n\ViewHelper\TranslateHtml',
+            $unit->createService($viewSm)
+        );
+    }
+}
