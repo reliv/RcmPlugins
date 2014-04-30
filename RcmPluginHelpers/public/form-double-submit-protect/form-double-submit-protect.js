0318a8066f92cee583022181a296b07b7b084fbb
Rod McNew <rmcnew@relivinc.com>
2014-04-30 17:37:19 -0500

    angular preiew totals problems fixed   WEBCART-242
        Convert cart page to combined-modular checkout system for PWS

--- RcmPluginHelpers/public/form-double-submit-protect/form-double-submit-protect.js
+++ RcmPluginHelpers/public/form-double-submit-protect/form-double-submit-protect.js
@@ -12,7 +12,6 @@ function formDoubleClickSubmitProtectInit() {
                 return false;
             }
             this.className = this.className + ' processing';
-            console.log('submitted');
             this.submit();
             return true;
         });
