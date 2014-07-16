5fc4b992c5e37a127d3648dd1fda5382855c706b
Inna Davis <idavis@relivinc.com>
2014-07-16 17:38:00 -0500

    REF-158
    Using blank dialog class for dialog to show up
    Bootstrap template for page properties dialog
    Angular controller for Save button
    jQuery inside of Angular to update DOM's title, description and keywords on save action

---
+++ RcmAdmin/public/js/page-properties.js
@@ -0,0 +1,58 @@
+/**
+ * Created by idavis on 7/15/14.
+ */
+
+
+angular.module('rcmAdminPage', [])
+    .controller('PageProperties',
+    function ($scope) {
+        var self = this;
+        //getting title, description and keywords from dom to our form
+        $scope.title = $('title').html();
+        $scope.description = $('description').html();
+        $scope.keywords = $('keywords').html();
+        //save function
+        $scope.save = function () {
+
+            //if title tag doesn't exists then adding it to head
+            if ($('title').length == 0) {
+                $('head').append($('title'));
+            }
+            else {
+                $('title').html($scope.title);
+            }
+            var meta = $('<meta>');
+            var metaDesciption = $('meta[name="description"]');
+
+            //if meta description doesn't exists then adding it to head
+            if (metaDesciption.length == 0) {
+                meta.attr('name', 'description');
+                meta.attr('content', $scope.description);
+                $('head').append(meta);
+
+            }
+            else {
+                metaDesciption.attr('content', $scope.description);
+            }
+            var metaKeywords = $('meta[name="keywords"]');
+            var metaK = $('<meta>');
+            //if meta keywords doesn't exists then adding it to head
+            if (metaKeywords.length == 0) {
+                metaK.attr('name', 'keywords');
+                metaK.attr('content', $scope.keywords);
+                $('head').append(metaK);
+            }
+            else {
+                metaKeywords.attr('content', $scope.keywords);
+            }
+        }
+    }
+);
+angular.element(document).ready(
+    function () {
+        angular.bootstrap(
+            document.querySelectorAll('[ng-controller=PageProperties]'),
+            ['rcmAdminPage']
+        );
+    }
+);
