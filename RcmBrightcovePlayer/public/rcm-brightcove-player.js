bc47e59568dc7b9f4df30737b0a291fbaf506d7e
Rod McNew <rmcnew@relivinc.com>
2014-08-01 14:05:38 -0500

    fix save button css and remove all bootstraps

--- RcmBrightcovePlayer/public/rcm-brightcove-player.js
+++ RcmBrightcovePlayer/public/rcm-brightcove-player.js
@@ -1,4 +1,3 @@
-
 rcm.addAngularModule('playerTabs');
 var app = angular.module('playerTabs', [])
     .controller('playerTabsCtrl', function ($scope) {
@@ -35,5 +34,48 @@ var app = angular.module('playerTabs', [])
             window[apiObjectName].loadVideoById(this.video.id);
         };
 
-    })
+    }
+).controller(
+    'PlayerEditCtrl',
+    function PlayerEditCtrl($scope) {
 
+        $scope.init = function (instanceConfig) {
+
+            function fillDropdownList(items) {
+
+                $scope.videos = items;
+                $scope.selectedVideos = $scope.videos[0];
+                $scope.$apply();
+            }
+
+            singleEmbedDropdownList(fillDropdownList);
+
+            function processMultiselectResponse(data) {
+
+                var allOfPlaylists = data.items;
+                var selectedIds = instanceConfig['playlistIds'];
+                $scope.unselectedPlaylist = [];
+                $scope.selectedPlaylist = [];
+                $.each(allOfPlaylists, function () {
+
+                    var pos = $.inArray(this.id, selectedIds);
+                    if (pos == -1) {
+                        $scope.unselectedPlaylist.push(this);
+                    } else {
+                        $scope.selectedPlaylist.push(this);
+                    }
+                });
+
+                $scope.$apply();
+
+            }
+
+            requestPlaylist(processMultiselectResponse);
+        };
+
+        $scope.items = [
+            { id: 'single-embed', name: 'single embed' },
+            { id: 'multi-embed', name: 'tabbed video player' }
+        ];
+    }
+);
