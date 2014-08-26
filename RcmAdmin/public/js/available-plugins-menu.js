9f906e461e8540b7acb78020550faa7fdb1d4b2d
Rod McNew <rmcnew@relivinc.com>
2014-08-26 14:02:43 -0500

    fix ie8

--- RcmAdmin/public/js/available-plugins-menu.js
+++ RcmAdmin/public/js/available-plugins-menu.js
@@ -1,58 +1,52 @@
 var RcmAvailablePluginsMenu = {
 
     build: function () {
-        console.log('poop');
-        $(function () {
-            var menu = $('<div class="availablePluginsMenu panel panel-default">');
-            $('body').prepend(menu);
+        var menu = $('<div class="availablePluginsMenu panel panel-default"></div>');
+        $('body').prepend(menu);
+        menu.css('top', $('.rcmAdminPanelWrapper').height());
+        menu.append('<h1 class="panel-heading">Available Plugins</h1>');
+        var accordion = $('<div class="panel-group" id="availablePluginsGroup">');
+        menu.append(accordion);
+        var categoryIndex = 0;
+        $.each(window.rcmAvailablePlugins, function (category, plugins) {
+            var collapseId = 'availablePluginsCollapse' + categoryIndex;
 
-            menu.css('top', $('.rcmAdminPanelWrapper').height());
-            menu.append('<h1 class="panel-heading">Available Plugins</h1>');
-            var accordion = $('<div class="panel-group" id="availablePluginsGroup">');
-            menu.append(accordion);
-            menu.draggable();
-            var categoryIndex = 0;
+            var group = $('<div class="panel panel-default"></div>');
+            group.appendTo(accordion);
 
-            $.each(window.rcmAvailablePlugins, function (category, plugins) {
-                var collapseId = 'availablePluginsCollapse' + categoryIndex;
+            var link = $('<a class="panel-link"></a>');
+            link.appendTo(group);
+            link.attr('data-parent', '#availablePluginsGroup');
+            link.attr('data-toggle', 'collapse');
+            link.attr('href', '#' + collapseId);
 
-                var group = $('<div class="panel panel-default">');
-                group.appendTo(accordion);
+            var heading = $('<div class="panel-heading"></div>');
+            heading.appendTo(link);
 
-                var link = $('<a class="panel-link">');
-                link.appendTo(group);
-                link.attr('data-parent', '#availablePluginsGroup');
-                link.attr('data-toggle', 'collapse');
-                link.attr('href', '#' + collapseId);
+            var title = $('<h4 class="panel-title"></h4>');
+            title.appendTo(heading);
+            title.html(category);
 
-                var heading = $('<div class="panel-heading">');
-                heading.appendTo(link);
+            var collapse = $('<div class="panel-collapse collapse"></div>');
+            collapse.appendTo(group);
+            collapse.attr('id', collapseId);
 
-                var title = $('<h4 class="panel-title">');
-                title.appendTo(heading);
-                title.html(category);
+            var collapseBody = $('<div class="panel-body"></div>');
+            collapse.append(collapseBody);
 
-                var collapse = $('<div class="panel-collapse collapse">');
-                collapse.appendTo(group);
-                collapse.attr('id', collapseId);
+            $.each(plugins, function (name, pluginInfo) {
+                var plugin = $('<div class="rcmPluginDrag panel-inner"></div>');
+                plugin.appendTo(collapseBody);
+                plugin.data('pluginName', name);
 
-                var collapseBody = $('<div class="panel-body">');
-                collapse.append(collapseBody);
-
-                $.each(plugins, function (name, pluginInfo) {
-                    var plugin = $('<div class="rcmPluginDrag panel-inner">');
-                    plugin.appendTo(collapseBody);
-                    plugin.data('pluginName', name);
-
-                    var icon = $('<img>');
-                    icon.attr('src', pluginInfo.icon);
-                    icon.appendTo(plugin);
-                    var displayName = $('<span>');
-                    displayName.appendTo(plugin);
-                    displayName.html(pluginInfo.displayName);
-                });
-                categoryIndex++;
+                var icon = $('<img>');
+                icon.attr('src', pluginInfo.icon);
+                icon.appendTo(plugin);
+                var displayName = $('<span></span>');
+                displayName.appendTo(plugin);
+                displayName.html(pluginInfo.displayName);
             });
+            categoryIndex++;
         });
     }
 };
