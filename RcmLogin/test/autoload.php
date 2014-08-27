e94ff8e8c9fef327c031fdb7d70af03f2af1397d
Rod McNew <rmcnew@relivinc.com>
2014-08-27 10:59:06 -0500

    fix build

--- RcmLogin/test/autoload.php
+++ RcmLogin/test/autoload.php
@@ -26,7 +26,7 @@ for ($i = 1; $i < 20; $i++) {
     $dots .= '../';
     if (file_exists(__DIR__ . '/' . $dots . 'vendor/autoload.php')) {
         //Get the composer autoloader from vendor folder as a standalone module
-        $autoload = __DIR__ . '/../../../../autoload.php';
+        $autoload = __DIR__ . '/' . $dots . 'vendor/autoload.php';
         break;
     }
 }
