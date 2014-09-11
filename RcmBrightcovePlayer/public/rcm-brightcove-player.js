ebaaec70b337079ee4213ca9c8034e35fa6d040f
James Jervis <jjervis@relivinc.com>
2014-09-11 13:19:06 -0500

    Refactor Brightcove js

--- RcmBrightcovePlayer/public/rcm-brightcove-player.js
+++ RcmBrightcovePlayer/public/rcm-brightcove-player.js
@@ -39,12 +39,15 @@ angular.module('playerTabs', [])
                     compile: function (tElm, tAttrs) {
 
                         var instanceId = tAttrs.rcmBrightcovePlayer;
+                        var instanceConfig = JSON.parse(tAttrs.rcmBrightcovePlayerConfig);
+
                         var objectElm = tElm.children(":first");
 
-                        var playerController = RcmBrightcovePlayerService.getPlayerController(
+                        var playerController = RcmBrightcovePlayerService.buildPlayerController(
                             instanceId,
+                            instanceConfig,
                             function () {
-
+                                console.log('rcmBrightcovePlayer build complete');
                             }
                         );
 
@@ -64,17 +67,7 @@ angular.module('playerTabs', [])
 
                         return function (scope, elm, attrs) {
 
-                            RcmBrightcovePlayerService.eventManager.on(
-                                'cueVideo',
-                                function (videoId) {
-
-                                    console.log('rcmBrightcovePlayer.on.cueVideo' + videoId);
-                                    scope.$apply();
-                                }
-                            );
-
                             scope.playerController = playerController;
-
                         };
                     },
 
@@ -92,6 +85,8 @@ angular.module('playerTabs', [])
                         ' <param name="mute" value="true"/>' +
                         ' <param name="secureConnections" value="true"/>' +
                         ' <param name="secureHTMLConnections" value="true"/>' +
+                        ' <param name="wmode" value="opaque"/>' +
+                        ' <param name="includeAPI" value="true" />' +
                         //' <param name="@videoPlayer" value="{{playerController.videoId}}"/>' +
                         //' <param name="templateLoadHandler" value="RcmBrightcovePlayerService.getPlayerController(\'' + instanceId + '\').onTemplateLoad"/>' +
                         //' <param name="templateReadyHandler" value="RcmBrightcovePlayerService.getPlayerController(\'' + instanceId + '\').onTemplateReady"/>' +
@@ -111,21 +106,18 @@ angular.module('playerTabs', [])
 
                 var instanceId = tAttr.playerTabsCtrl;
 
-                console.log('playerTabsCtrl--', instanceId);
-
                 RcmBrightcovePlayerService.eventManager.on(
                     'playlistsBuilt',
                     function (playerCtrl) {
 
-                        console.log('on.playlistsBuilt');
                         scope.playlists = playerCtrl.playlists;
 
                         if (instanceId == playerCtrl.instanceId) {
 
                             scope.$apply(
-                                function(){
+                                function () {
                                     setTimeout(function () {
-                                        console.log('playerTabsCtrl.apply', elm);
+                                        console.log('playerTabsCtrl.on.playlistsBuilt');
                                         elm.find('#rcm-brightcove-player-tabs').tabs();
                                     });
                                 }
@@ -154,47 +146,6 @@ angular.module('playerTabs', [])
             compile: compile,
             restrict: 'A'
         };
-    }
-)
-
-    .controller(
-    'playerTabsCtrl',
-    function ($scope) {
-
-        var instanceId;
-
-        $scope.playlists = [];
-
-        $scope.init = function (playlistIds, passedInstanceId) {
-            console.log('playerTabsCtrl.init');
-            instanceId = passedInstanceId;
-            RcmBrightcoveApiService.findPlaylistsById(
-                playlistIds,
-                renderTabs
-            );
-        };
-
-        function renderTabs(data) {
-
-            console.log('playerTabsCtrl.renderTabs');
-            $scope.playlists = data.items;
-            var firstVideo = $scope.playlists[0].videos[0];
-            window['bgPlayerApi' + instanceId].cueVideoById(firstVideo.id);
-
-            $scope.$apply(
-                function () {
-
-                    $('#rcm-brightcove-player-tabs').tabs();
-                }
-            );
-        }
-
-        $scope.videoClick = function () {
-            console.log('playerTabsCtrl.videoClick');
-            var apiObjectName = 'bgPlayerApi' + instanceId;
-            window[apiObjectName].loadVideoById(this.video.id);
-        };
-
     }
 )
     .controller(
@@ -285,11 +236,26 @@ var RcmBrightcovePlayerService = {
         }
     },
 
-    addConfig: function (instanceId, instanceConfig) {
+//    addConfig: function (instanceId, instanceConfig, onComplete) {
+//
+//
+//
+//        RcmBrightcovePlayerService.buildPlayerController(instanceId, instanceConfig);
+//    },
+
+    buildPlayerController: function (instanceId, instanceConfig, onComplete) {
 
         instanceConfig.playerConfig = RcmBrightcovePlayerService.playerConfig;
 
-        RcmBrightcovePlayerService.configs[instanceId] = instanceConfig;
+        if (instanceConfig.type == 'multi-embed') {
+
+            RcmBrightcovePlayerService.playerControllers[instanceId] = new RcmBrightcovePlayerMulti(instanceId, instanceConfig, onComplete);
+        } else {
+
+            RcmBrightcovePlayerService.playerControllers[instanceId] = new RcmBrightcovePlayerSingle(instanceId, instanceConfig, onComplete);
+        }
+
+        return RcmBrightcovePlayerService.playerControllers[instanceId];
     },
 
     getPlayerController: function (instanceId, onComplete) {
@@ -304,23 +270,8 @@ var RcmBrightcovePlayerService = {
             return RcmBrightcovePlayerService.playerControllers[instanceId];
         }
 
-        if (!RcmBrightcovePlayerService.configs[instanceId]) {
-
-            console.warn('RcmBrightcovePlayerService: Config for ' + instanceId + ' not registered.')
-            return null;
-        }
-
-        var instanceConfig = RcmBrightcovePlayerService.configs[instanceId];
-
-        if (instanceConfig.type == 'multi-embed') {
-
-            RcmBrightcovePlayerService.playerControllers[instanceId] = new RcmBrightcovePlayerMulti(instanceId, instanceConfig, onComplete);
-        } else {
-
-            RcmBrightcovePlayerService.playerControllers[instanceId] = new RcmBrightcovePlayerSingle(instanceId, instanceConfig, onComplete);
-        }
-
-        return RcmBrightcovePlayerService.playerControllers[instanceId];
+        console.warn('RcmBrightcovePlayerService: Config for ' + instanceId + ' not registered, use addConfig before get.')
+        return null;
     },
 
     playerConfig: {
@@ -352,6 +303,15 @@ var RcmBrightcovePlayerSingle = function (instanceId, instanceConfig, onComplete
             self.downloadUrl = url;
             RcmBrightcovePlayerService.eventManager.trigger('downloadUrlChange', self);
         }
+    };
+
+    self.onTemplateLoad = function (experienceID) {
+        console.log('RcmBrightcovePlayerSingle.onTemplateLoad')
+    };
+
+    self.onTemplateReady = function (evt) {
+        console.log('RcmBrightcovePlayerSingle.onTemplateReady')
+
     };
 
     self.init = function (onComplete) {
@@ -394,37 +354,35 @@ var RcmBrightcovePlayerMulti = function (instanceId, instanceConfig, onComplete)
     self.videoPlayer;
 
     self.playlists = [];
-    self.firstVideo = null;
 
     self.onTemplateLoad = function (experienceID) {
         console.log('onTemplateLoad: ' + experienceID);
 
-        player = brightcove.api.getExperience(experienceID);
-        //APIModules = brightcove.api.modules.APIModules;
-        console.log('heheheherheh');
-    };
+        self.player = brightcove.api.getExperience(experienceID);
+        self.APIModules = brightcove.api.modules.APIModules;
+        self.mediaEvent = brightcove.api.events.MediaEvent;
 
-    self.onTemplateReady = function (evt) {
-
-        videoPlayer = player.getModule(APIModules.VIDEO_PLAYER);
-
-        console.log('onTemplateReady: ');
-
-        console.log('onTemplateReady: loadVideoIdWhenReady', self.loadVideoIdWhenReady);
+        self.videoPlayer = self.player.getModule(self.APIModules.VIDEO_PLAYER);
+        self.videoPlayer.addEventListener(self.mediaEvent.BEGIN, self.onMediaBegin);
+        self.videoPlayer.addEventListener(self.mediaEvent.COMPLETE, self.onMediaComplete);
 
         self.templateReady = true;
-        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.BEGIN, self.onMediaBegin);
-        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.COMPLETE, self.onMediaComplete);
 
-        console.log('onTemplateReady: loadVideoIdWhenReady', self.loadVideoIdWhenReady);
         if (self.loadVideoIdWhenReady) {
             self.cueVideoById(self.loadVideoIdWhenReady);
         }
     };
 
+    self.onTemplateReady = function (evt) {
+
+        console.log('onTemplateReady: ');
+    };
+
     self.loadVideoById = function (videoId, callback) {
 
-        videoPlayer.loadVideoByID(videoId);
+        console.log('loadVideoById',videoId);
+
+        self.videoPlayer.loadVideoByID(videoId);
 
         self.getDownloadURL(videoId, callback);
     };
@@ -434,25 +392,21 @@ var RcmBrightcovePlayerMulti = function (instanceId, instanceConfig, onComplete)
         console.log('cueVideoById: ' + videoId);
 
         if (self.templateReady) {
-            // onTemplateReady has already been called so we can just cue the video
-            if (self.loadVideoIdWhenReady == videoId) {
-                self.loadVideoIdWhenReady = null;
-            }
 
-            self.videoId = videoId;
-            videoPlayer.cueVideoByID(videoId);
-            self.getDownloadURL(videoId, callback);
-            console.log('cueVideoById.do: ' + videoId);
+            // onTemplateReady has already been called so we can just cue the video
+            self.videoPlayer.cueVideoByID(videoId);
             RcmBrightcovePlayerService.eventManager.trigger('cueVideo', videoId);
         } else {
+
             // onTemplateReady hasn't been called yet so we set this property to be used when it does get called
-            console.log('cueVideoById.save: ' + videoId);
             self.loadVideoIdWhenReady = videoId;
         }
+
+        self.getDownloadURL(videoId, callback);
     };
 
     self.onMediaBegin = function (evt) {
-        //document.getElementById("mediaInfo").innerHTML = evt.media.displayName;
+        //displayName  = evt.media.displayName;
         RcmBrightcovePlayerService.eventManager.trigger('onMediaBegin', self);
     }
 
@@ -461,8 +415,9 @@ var RcmBrightcovePlayerMulti = function (instanceId, instanceConfig, onComplete)
         if (nextVideo == videos.length) {
             nextVideo = 0;
         }
+
+        self.videoPlayer.loadVideoByID(videos[nextVideo]);
         RcmBrightcovePlayerService.eventManager.trigger('onMediaComplete', self);
-        videoPlayer.loadVideoByID(videos[nextVideo]);
     };
 
     self.getDownloadURL = function (videoId, callback) {
@@ -491,15 +446,20 @@ var RcmBrightcovePlayerMulti = function (instanceId, instanceConfig, onComplete)
 
         console.log('prepareData', data.items);
         self.playlists = data.items;
-        self.firstVideo = self.playlists[0].videos[0];
-
-        RcmBrightcovePlayerService.eventManager.trigger('playlistsBuilt', self);
+        // set videoId to play fo first video
+        self.videoId = self.playlists[0].videos[0].id;
 
         if (typeof callback === 'function') {
             callback(self);
         }
+
+        RcmBrightcovePlayerService.eventManager.trigger('playlistsBuilt', self);
     };
 
+    /**
+     * buildPlaylist
+     * @param onComplete
+     */
     self.buildPlaylist = function (onComplete) {
 
         RcmBrightcoveApiService.findPlaylistsById(
@@ -510,60 +470,25 @@ var RcmBrightcovePlayerMulti = function (instanceId, instanceConfig, onComplete)
         );
     };
 
+    /**
+     * init
+     * @param onComplete
+     */
     self.init = function (onComplete) {
 
         self.buildPlaylist(
-            function (player) {
+            function(thisPlayer) {
 
-                self.cueVideoById(player.firstVideo.id, onComplete);
+                if(thisPlayer.videoId){
+                    thisPlayer.cueVideoById(thisPlayer.videoId)
+                }
 
-                RcmBrightcovePlayerService.eventManager.trigger('init', self);
+                if (typeof onComplete === 'function') {
+                    onComplete(thisPlayer);
+                }
             }
         );
     };
 
     self.init(onComplete);
 };
-
-// TEST
-var MultiPlayer = new function () {
-    var player,
-        APIModules,
-        videoPlayer,
-        contentModule,
-        videosAra,
-        counter = 0;
-
-    this.onTemplateLoad = function (experienceID) {
-        player = brightcove.api.getExperience(experienceID);
-        APIModules = brightcove.api.modules.APIModules;
-    };
-
-    this.onTemplateReady = function (evt) {
-        videoPlayer = player.getModule(APIModules.VIDEO_PLAYER);
-        contentModule = player.getModule(APIModules.CONTENT);
-        contentModule.getPlaylistByID("1323984733001", onGetPlaylist);
-        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.COMPLETE, onComplete);
-    };
-
-    this.onComplete = function (evt) {
-        counter++;
-        var toPlayID = videosAra[counter].id;
-        videoPlayer.loadVideoByID(toPlayID);
-    }
-
-    this.onGetPlaylist = function (playlistDTO) {
-        videosAra = playlistDTO.videos;
-        videoPlayer.loadVideoByID(videosAra[0].id);
-        for (var i = 0; i < playlistDTO.videoCount; i++) {
-            videoSelect.innerHTML += '<option value="' + playlistDTO.videos[i].id + '">' + playlistDTO.videos[i].displayName + '</option>';
-        }
-    };
-
-    this.onVideoSelected = function (evt) {
-        var selectedIndex = videoSelect.selectedIndex;
-        counter = selectedIndex;
-        var toPlayID = videosAra[selectedIndex].id;
-        videoPlayer.loadVideoByID(toPlayID);
-    };
-}
