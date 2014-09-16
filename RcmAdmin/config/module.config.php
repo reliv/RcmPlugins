72bc19e78a3efcbec3029e7911908f66fc410919
Inna Davis <idavis@relivinc.com>
2014-09-16 14:44:47 -0500

    fixPagePermission

--- RcmAdmin/config/module.config.php
+++ RcmAdmin/config/module.config.php
@@ -52,7 +52,7 @@ return array(
                                 'class' => 'RcmAdminMenu RcmBlankDialog',
                                 'uri' => '/modules/rcm-admin/page-properties.html',
                             ),
-                            'PagePemissions' => array(
+                            'PagePermissions' => array(
                                 'label' => 'Page Permissions',
                                 'class' => 'RcmAdminMenu RcmBlankDialog',
                                 'uri' => '/modules/rcm-admin/page-permissions.html',
