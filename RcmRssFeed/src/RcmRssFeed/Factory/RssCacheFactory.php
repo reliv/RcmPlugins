39623213a615845094eee637f7303a9a5f460289
Westin Shafer <wshafer@relivinc.com>
2014-09-20 10:51:28 -0500

    Fix to Application Config Cache Fix for Rss Feed Reader Cache Added setting the service locator for plugin controllers

---
+++ RcmRssFeed/src/RcmRssFeed/Factory/RssCacheFactory.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Service Factory for the Rss Proxy Cache
+ *
+ * This file contains the factory needed to generate a Rss Proxy Cache.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: BSD
+ *
+ * @category  Reliv
+ * @package   RcmPlugins
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+namespace RcmRssFeed\Factory;
+
+use Zend\Cache\StorageFactory;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Service Factory for the Rss Proxy Cache
+ *
+ * Factory for the Rss Proxy Cache.
+ *
+ * @category  Reliv
+ * @package   RcmPlugins
+ * @author    Westin Shafer <wshafer@relivinc.com>
+ * @copyright 2012 Reliv International
+ * @license   License.txt New BSD License
+ * @version   Release: 1.0
+ * @link      https://github.com/reliv
+ *
+ */
+class RssCacheFactory implements FactoryInterface
+{
+
+    /**
+     * Create Service
+     *
+     * @param ServiceLocatorInterface $serviceLocator Zend Service Manager
+     *
+     * @return \Zend\Cache\Storage\StorageInterface
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('config');
+
+        $extraOptions = array(
+            'namespace' => 'rcmRssCache',
+            'ttl' => '300'
+        );
+
+        $cache = StorageFactory::factory(
+            array(
+                'adapter' => array(
+                    'name' => $config['rcmCache']['adapter'],
+                    'options' => $config['rcmCache']['options']
+                        + $extraOptions,
+                ),
+                'plugins' => $config['rcmCache']['plugins'],
+            )
+        );
+
+        return $cache;
+    }
+}
