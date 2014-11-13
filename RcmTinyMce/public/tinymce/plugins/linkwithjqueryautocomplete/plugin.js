434d5ada95f8841a345dd2a9cd4de9c24990826f
Rod McNew <rmcnew@relivinc.com>
2014-11-13 10:53:27 -0600

    fix tiny mce

--- RcmTinyMce/public/tinymce/plugins/linkwithjqueryautocomplete/plugin.js
+++ RcmTinyMce/public/tinymce/plugins/linkwithjqueryautocomplete/plugin.js
@@ -5,6 +5,10 @@
  * has been replaced by a jQuery-ui auto-complete menu on the url text box so
  * you can just start typing rather than scrolling through the "link list"
  * select box.
+ *
+ * You must have jQuery-ui installed and included for this to work.
+ *
+ * Provide your link list data to tiny mce in the standard way in config
  * 
  * These changes were made by Rod McNew. Rod Mcnew's changes to this file are
  * free for anyone to use for anything they want. Please check the licensing
