bd4063459ed46c8011cbe5a361cba89e6407ce17
Rod McNew <rmcnew@relivinc.com>
2015-02-11 14:25:43 -0600

    js error logger client

--- RcmErrorHandler/public/js-error-logger.js
+++ RcmErrorHandler/public/js-error-logger.js
@@ -1,13 +1,14 @@
-window.onerror = function (message, file, line) {
+window.onerror = function (message, file, line, column, errorObj) {
     // Supports IE7+, Firefox, Chrome, Opera, Safari
     if (window.XMLHttpRequest) {
+        var error = new Error();
         var http = new XMLHttpRequest();
         http.open('POST', '/api/rcm-error-handler/js-error', true);
         http.setRequestHeader('Content-Type', 'application/json');
         http.send(JSON.stringify({
             message: message,
             file: file,
-            line: line
+            line: line + ':' + column
         }));
     }
 };
