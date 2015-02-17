fe60d34737cfa7b2bbe2671992dacdde4c7a7778
Inna Davis <idavis@relivinc.com>
2015-02-17 16:23:08 -0600

    Brighcove multiplayer changes in style, bootstrap

--- RcmBrightcovePlayer/config/defaultInstanceConfig.php
+++ RcmBrightcovePlayer/config/defaultInstanceConfig.php
@@ -5,14 +5,4 @@ return [
     'playlistIds' => [1519038999001, 1519039006001],
     'type' => 'single-embed',
     'download' => 'Right-click here and select "Save as" to download video',
-    'containers' => [
-        [
-            'id' => 1, 
-            'type' => 'html'
-        ],
-    ],
-    'alwaysShowAccordionContent' => true,
-    'tab_title_1' => 'New Tab',
-    'tab_content_1' => '<h1>Lorem ipsum</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justosapien, convallis vehicula sollicitudin tristique, rhoncus vel enim.Maecenas mollis dignissim urna, et mollis augue tempus nec. Proin a orcinulla. Pellentesque laoreet orci vitae nisl viverra imperdiet. Nam eueuismod orci. Etiam adipiscing condimentum quam a venenatis. Fusce faucibuslacus non velit varius egestas. Nam molestie mattis sem quis cursus. Duissit amet nunc turpis. Mauris elit urna, dapibus id hendrerit eleifend,placerat at magna. Etiam ornare eleifend elit, vel blandit tellussagittis pretium. Vivamus ut dignissim purus.</p>',
-
 ];
