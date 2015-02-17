29d1039fd63b8864cf883d6d7144bee87679278f
Inna Davis <idavis@relivinc.com>
2015-02-17 16:24:24 -0600

    Brighcove multiplayer changes in style, bootstrap

--- RcmBrightcovePlayer/config/defaultInstanceConfig.php
+++ RcmBrightcovePlayer/config/defaultInstanceConfig.php
@@ -4,5 +4,5 @@ return [
     'videoId' => '2939867528001',
     'playlistIds' => [1519038999001, 1519039006001],
     'type' => 'single-embed',
-    'download' => 'Right-click here and select "Save as" to download video',
+    'download' => 'Right-click here and select "Save as" to download video'
 ];
