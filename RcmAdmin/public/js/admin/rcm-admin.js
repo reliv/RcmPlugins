f88b803bfb4f0fcc0c22d299520ac0e1672ef0b6
Rod McNew <rmcnew@relivinc.com>
2015-02-25 09:43:30 -0600

    row changes

--- RcmAdmin/public/js/admin/rcm-admin.js
+++ RcmAdmin/public/js/admin/rcm-admin.js
@@ -511,7 +511,7 @@ var RcmAdminService = {
      * getPlugin
      * @param id
      * @param onComplete
-     * @returns {*}
+     * @returns {RcmPlugin}
      */
     getPlugin: function (id, onComplete) {
 
