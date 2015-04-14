4006ab25732dc8787e2eaf096747e0591055451c
James Jervis <jjervis@relivinc.com>
2015-04-14 12:29:03 -0500

    Add keep alive service

--- RcmAdmin/public/rcm-page-admin/rcm-session-keep-alive.js
+++ RcmAdmin/public/rcm-page-admin/rcm-session-keep-alive.js
@@ -2,21 +2,28 @@
  * Created by idavis on 9/10/14.
  */
 
-    (function() {
-        var page = RcmAdminService.getPage();
-        var sessionKeepAlive = false;
-        page.events.on(
-            'editingStateChange',
-            function (page) {
-                if (page.editMode === true && !sessionKeepAlive) {
-                    //preventing from session keep alive on multiple pages
-                    sessionKeepAlive = true;
-                    setInterval(
-                        function () {
-                            $.get('/rcm-page-search/title');
-                }, 300000
-                    );
-                }
+(function () {
+    var page = RcmAdminService.getPage();
+    var sessionKeepAlive = false;
+    page.events.on(
+        'editingStateChange',
+        function (page) {
+            if (page.editMode === true && !sessionKeepAlive) {
+                //preventing from session keep alive on multiple pages
+                sessionKeepAlive = true;
+                setInterval(
+                    function () {
+                        var timestamp = Math.floor(new Date().getTime() / 1000);
+                        $.post(
+                            '/api/rpc/rcm-admin/keep-alive',
+                            {'requestTime': timestamp},
+                            function(data){
+                                //console.log('keep-alive',data);
+                            }
+                        );
+                    }, 300000
+                );
             }
-        );
-    })();
+        }
+    );
+})();
