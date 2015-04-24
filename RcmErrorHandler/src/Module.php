c19a373f0032820090dabc5e629c0f45aa6e9346
Rod McNew <rmcnew@relivinc.com>
2015-04-24 16:07:45 -0500

    fix composer

--- RcmErrorHandler/src/Module.php
+++ RcmErrorHandler/src/Module.php
@@ -29,7 +29,7 @@ class Module implements AutoloaderProviderInterface
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function onBootstrap(MvcEvent $e)
--- RcmNavResponsive/Module.php
+++ RcmNavResponsive/Module.php
@@ -42,7 +42,7 @@ class Module
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
