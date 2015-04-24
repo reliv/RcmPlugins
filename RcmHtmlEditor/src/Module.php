a40b0bc154a128ca7ddb189b451280c10f0b04c3
Westin Shafer <wshafer@relivinc.com>
2015-04-24 16:42:35 -0500

    Update to composer autoloader and update to PSR 4

---
+++ RcmHtmlEditor/src/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RcmHtmlEditor;
+
+/**
+ * ZF2 Module Config.  Required by ZF2
+ *
+ * ZF2 requires a Module.php file to load up all the Module Dependencies.  This
+ * file has been included as part of the ZF2 standards.
+ */
+class Module
+{
+    /**
+     * getConfig() is a requirement for all Modules in ZF2.  This
+     * function is included as part of that standard.  See Docs on ZF2 for more
+     * information.
+     *
+     * @return array Returns array to be used by the ZF2 Module Manager
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
---
+++ RcmMockPlugin/src/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RcmMockPlugin;
+
+class Module
+{
+    /**
+     * getConfig() is a requirement for all Modules in ZF2.  This
+     * function is included as part of that standard.  See Docs on ZF2 for more
+     * information.
+     *
+     * @return array Returns array to be used by the ZF2 Module Manager
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
